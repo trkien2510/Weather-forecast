86f2a01584d97472130974c4910d4723b00863b7
trkien2510 <trkien2503@gmail.com>
2024-09-14 14:38:34 +0700

    update

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ const weatherDescriptions = {
     'mist': 'Sương mù'
 };
 
-firstUpdate();                      //Gọi hàm lấy thời gian hiện tại
-setInterval(firstUpdate, 1000);     //Gọi lại hàm lấy thời gian mỗi 1s(1000 mili giây)
+firstUpdate();
+setInterval(firstUpdate, 1000);
 
 function firstUpdate() {
     const d = new Date();
@@ -46,10 +46,7 @@ function firstUpdate() {
     dateEl.innerHTML = dateString;
 }
 
-//Đặt button nhiệt độ c là mặc định khi load xong
 document.getElementById('c').classList.add('selected');
-//Thực hiện các hành động khi nhấn button c, chuyển C_or_F về 'metric' để API trả về thông tin với độ c
-//Gọi lại hoác hàm để cập nhật thông tin
 document.getElementById('c').addEventListener('click', function () {
     C_or_F = 'metric';
     noteTemp = '°C';
@@ -61,8 +58,6 @@ document.getElementById('c').addEventListener('click', function () {
     getInformation();
 });
 
-//Thực hiện các hành động khi nhấn button f, chuyển C_or_F về 'imperial' để API trả về thông tin với độ f
-//Gọi lại hoác hàm để cập nhật thông tin
 document.getElementById('f').addEventListener('click', function () {
     C_or_F = 'imperial';
     noteTemp = '°F';
@@ -74,20 +69,16 @@ document.getElementById('f').addEventListener('click', function () {
     getInformation();
 });
 
-//Hàm chờ wed load xong sẽ gọi hàm getLocation();
 document.addEventListener('DOMContentLoaded', (event) => {
     getLocation();
 });
 
-//kiểm tra trình duyệt có hỗ trợ API địa lý không. Nếu có thì hàm getCurrentPosition() được gọi để lấy thông tin vị trí hiện tại
-//Khi lấy được vị trí, nó sẽ gọi đến hàm getWeatherByLocation
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getWeatherByLocation);
     }
 }
 
-//Nhận thông tin vị trí và chuyển thành vĩ độ và kinh độ.
 function getWeatherByLocation(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
@@ -95,7 +86,6 @@ function getWeatherByLocation(position) {
     getInformation(url);
 }
 
-//tìm kiến tên thành phố bằng phím Enter
 document.getElementById('input_city_name').addEventListener('keydown', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
@@ -106,15 +96,13 @@ document.getElementById('input_city_name').addEventListener('keydown', function 
     }
 });
 
-//tìm kiến tên thành phố bằng nút tìm kiếm
 buttonEl.addEventListener('click', function () {
+    const cityName = document.getElementById('input_city_name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}&units=${C_or_F}`;
     document.getElementById('input_city_name').value = "";
     getInformation(url);
 });
 
-//Nhận url và tạo kết nối tới đường dẫn để nhận thông tin thời tiết
-//nếu không nhận được url  thì tạo kết nối với đường dẫn đã có
 async function getInformation(url = null) {
     try {
         if (!url) {
@@ -126,7 +114,6 @@ async function getInformation(url = null) {
         latitude = data.coord.lat.toFixed(2);
         longitude = data.coord.lon.toFixed(2);
 
-        //chuyển đuôi kinh độ, vĩ độ để sử dụng khi mở bản đồ
         if (latitude < 0 && longitude < 0) {
             lat = Math.abs(latitude) + 'S'
             lon = Math.abs(longitude) + 'W';
@@ -142,12 +129,10 @@ async function getInformation(url = null) {
         }
         const newMapUrl = `https://www.google.com/maps/place/${lat},${lon}`;
 
-        //Lấy thông tin mã thời tiết để hiển thị biểu tượng thời tiết và thay đổi background tương ứng
         const iconCode = data.weather[0].icon;
         const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
         changeBackground(iconCode);
 
-        //Sửa lỗi tìm kiếm tỉnh đà nẵng
         const true_name = data.name;
         if (true_name == 'Turan') {
             document.getElementById('city').textContent = 'Da nang';
@@ -173,7 +158,6 @@ async function getInformation(url = null) {
     }
 }
 
-// Nhận mã thời tiết để thay đổi background
 function changeBackground(iconCode) {
     let bgImageUrl;
 
@@ -221,7 +205,6 @@ function changeBackground(iconCode) {
     document.body.style.backgroundImage = bgImageUrl;
 }
 
-//Dự báo thời tiết 24h tới (3h 1 lần)
 async function getHourlyForecast() {
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_Key}&units=${C_or_F}`;
 
@@ -250,7 +233,6 @@ async function getHourlyForecast() {
     }
 }
 
-//Dự báo thời tiết 5 ngày tới(lấy thông tin dự báo vào 12:00 trưa để hiện thị)
 async function getDailyForecast() {
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_Key}&units=${C_or_F}`;
     let index = 1;
